ba0c32de137cfb8f3ad8e07355110af115f4cac3
Electro Hobby 3D <electrohobby3d@gmail.com>
2026-08-19 17:48:47 +0200

    FIXED FILES

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         versionName = "1.0"
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     buildFeatures {
         compose = true
